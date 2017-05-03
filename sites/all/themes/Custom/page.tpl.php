692d449484e3d1a1747bda490dbda85ee9bae27d
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-05-03 16:49:14 -0700

    add regions to custome.info and page .tpl.

---
+++ sites/all/themes/Custom/page.tpl.php
@@ -0,0 +1,39 @@
+<div id="root">
+
+    <div class="header-wrap">
+        <div class="header">
+
+        </div>
+    </div>
+
+    <div class="slide-show-wrap">
+        <div class="slide-show">
+
+        </div>
+    </div>
+
+    <div class="content-wrap">
+        <div class="content">
+
+        </div>
+    </div>
+
+    <div class="side-bar-first-wrap">
+        <div class="side-bar-first">
+
+        </div>
+    </div>
+
+    <div class="side-bar-second-wrap">
+        <div class="side-bar-second">
+
+        </div>
+    </div>
+    
+    <div class="header-wrap">
+        <div class="header">
+
+        </div>
+    </div>
+
+</div>
